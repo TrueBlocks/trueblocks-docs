eed31d9447facbf0f707420f6edcb4d1a42655e1
tjayrush <jrush@quickblocks.io>
2021-05-08 09:09:55 -0400

    Linking in API doc

--- rdme-cat.sh
+++ rdme-cat.sh
@@ -164,6 +164,6 @@ curl -s $READMES/tools/ethslurp/README.md >> $OTHER
 #----------------------------------
 APIHELP=$CONTENT/../help/api.html
 echo "Building api help file"
-curl -s $SOURCE/api.html >&APIHELP
+curl -s $SOURCE/api.html >$APIHELP
 
 echo "Pages built. To see what's changed, run  git diff content/docs/chifra"
