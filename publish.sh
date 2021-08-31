4caa29e9f47bed5c4f8d5cf566670991564932cd
Matt Dodson <47385188+MattDodsonEnglish@users.noreply.github.com>
2021-08-31 17:40:59 -0300

    publish; exits when things go wrong

--- publish.sh
+++ publish.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
 
+[ -z "$1" ] && echo "Add ssh name..." && exit 1
+
 USER=$1
 
 # build the static site into ./public folder
-hugo
+hugo || echo "The build failed. Exiting..." && exit 2 
 
 # touch all the files just because
 find public -exec touch {} ';'
 
 # copy the files to the server
-rsync -rv --update public/ $USER@trueblocks.io:/home/$USER/Websites/trueblocks.io/
+rsync -rv --update public/ "$USER"@trueblocks.io:/home/"$USER"/Websites/trueblocks.io/
 
