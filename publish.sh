d38c8c8196bbac80bb1c4496be8a3f9c989fe805
Matt Dodson <47385188+MattDodsonEnglish@users.noreply.github.com>
2021-09-01 15:08:37 -0300

    Publish; disables goog fonts, adds script warnings

--- publish.sh
+++ publish.sh
@@ -1,14 +1,30 @@
 #!/usr/bin/env bash
 
-[ -z "$1" ] && echo "Add ssh name..." && exit 1
+[ -z "$1" ] \
+  && echo "Add ssh name..." && exit 1
+
+if ! which redoc-cli > /dev/null; then
+  echo "You need redoc-cli to build the API docs. Skipping API..."
+  else
+    redoc-cli bundle \
+    --options.disableGoogleFont=true\
+    content/api/openapi.yaml -o content/api/index.html
+fi
+  
+
+if ! which rsync > /dev/null; then
+	echo "You need rysnc to run this script"
+        exit 2
+fi
 
 USER=$1
 
 # build the static site into ./public folder
-hugo || echo "The build failed. Exiting..." && exit 2 
+hugo --cleanDestinationDir \
+|| { echo "The build failed. Exiting..." ; exit 3; } 
 
 # touch all the files just because
-find public -exec touch {} ';'
+#find public -exec touch {} ';'
 
 # copy the files to the server
 rsync -rv --update public/ "$USER"@trueblocks.io:/home/"$USER"/Websites/trueblocks.io/
